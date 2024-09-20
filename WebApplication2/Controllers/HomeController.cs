2e4723d37523ede0b0c96b94961ff59b3b0f9f55
kevinggundersen <159529468+kevinggundersen@users.noreply.github.com>
2024-09-20 23:52:35 +0200

    Added Login/Register and an admin page Added links to these pages in the navbar

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -23,6 +23,21 @@ namespace WebApplication2.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Admin()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
