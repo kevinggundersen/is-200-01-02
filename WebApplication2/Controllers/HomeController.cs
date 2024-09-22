fa133eb05c914ba300a231c17d81cbc1c31f84d6
kevinggundersen <159529468+kevinggundersen@users.noreply.github.com>
2024-09-23 01:17:26 +0200

    Added individual submission page Added images to landing pages (need yo be changed)

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace WebApplication2.Controllers
             return View();
         }
 
+        public IActionResult AdminSubmission()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
